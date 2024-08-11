9405e995a95277ce363ad0351c671bcd625776d6
Mathieu-Hallez <145336820+Mathieu-Hallez@users.noreply.github.com>
2024-08-11 17:48:36 +0200

    Update karma.conf.js

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -30,7 +30,8 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'lcov' }
+        { type: 'lcov' },
+        { type: 'cobertura ' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
